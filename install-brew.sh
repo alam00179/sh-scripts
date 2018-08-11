9d52e2b78b457e9553e7d4c19d6eb99d5914ecf3
Khairul Alam <alam.00179@gmail.com>
2018-08-11 21:41:35 +0100

    added all files by git add *

---
+++ install-brew.sh
@@ -0,0 +1,17 @@
+
+
+:' 
+Homebrew
+Homebrew calls itself The missing package manager for macOS and is an essential tool for any developer.
+
+Installation
+Before you can run Homebrew you need to have the Command Line Tools for Xcode installed. It include compilers that will allow you to build things from source, and if you are missing this it's available through the App Store > Updates.
+
+To install Homebrew run the following: terminal, hit Enter, and follow the steps on the screen:
+'
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+
+
